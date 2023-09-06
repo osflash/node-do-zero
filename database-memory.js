88f209c0065875def6c1c12a9d0b2bd92ca4ecd1
OsFlash <felipedasilvatomaz1996@gmail.com>
2023-09-06 17:36:53 -0300

    API Node com Postgres

---
+++ database-memory.js
@@ -0,0 +1,33 @@
+import { randomUUID } from "node:crypto";
+
+export class DatabaseMemory {
+  #videos = new Map();
+
+  list(search) {
+    return Array.from(this.#videos.entries())
+      .map(([id, rest]) => {
+        return { id, ...rest };
+      })
+      .filter((videos) => {
+        if (search) {
+          return videos.title.includes(search);
+        }
+
+        return true;
+      });
+  }
+
+  create(video) {
+    const videoId = randomUUID();
+
+    this.#videos.set(videoId, video);
+  }
+
+  update(id, video) {
+    this.#videos.set(id, video);
+  }
+
+  delete(id) {
+    this.#videos.delete(id);
+  }
+}
